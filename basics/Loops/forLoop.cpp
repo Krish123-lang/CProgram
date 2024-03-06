5e81e965cacfb2f28977ca2ad2b460c09848b823
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 23:12:15 +0545

    FOR loop

---
+++ basics/Loops/forLoop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	int n=9, i=1;
+	for(i=1;i<=10;i++)
+	{
+		cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+	}
+	return 0;
+}
+/*
+9 * 1 = 9
+9 * 2 = 18
+9 * 3 = 27
+9 * 4 = 36
+9 * 5 = 45
+9 * 6 = 54
+9 * 7 = 63
+9 * 8 = 72
+9 * 9 = 81
+9 * 10 = 90
+*/
