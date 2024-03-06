3352d21c41906ba4213e72bc594eb5877f58f992
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 20:06:11 +0545

    arithmeticOps

---
+++ basics/Operators/arithmeticOps.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	float x=5.63, y=12.435;
+	
+	cout<<"Sum: "<<x+y<<endl;
+	cout<<"Subtract: "<<x-y<<endl;
+	cout<<"Product: "<<x*y<<endl;
+	cout<<"Divison: "<<x/y<<endl;
+	cout<<"Increament: "<<x++<<endl;
+	cout<<"Increament: "<<x++<<endl;
+	cout<<"Increament: "<<x++<<endl;
+
+	cout<<"Decreament: "<<x-- <<endl;
+	cout<<"Decreament: "<<x-- <<endl;
+	cout<<"Decreament: "<<x-- <<endl;
+	return 0;
+}
