e555df5d7189fd9c4a82f3d88024cca063c0670a
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 19:38:06 +0545

    userInput

---
+++ basics/userInput.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	int age;
+	
+	cout<<"Enter your age: ";
+	cin>>age;
+
+	cout<<"Your age is: "<< age;
+	*/
+
+	int x,y;
+	
+	cout<<"Enter x: ";
+	cin>>x;
+
+	cout<<"Enter y: ";
+	cin>>y;
+
+	cout<<"Sum: "<<x+y<<"\n";
+	cout<<"Subtraction: "<<x-y<<"\n";
+	cout<<"Product: "<<x*y<<"\n";
+	cout<<"Divide: "<<x/y<<"\n";
+
+	return 0;
+}
+/*
+Enter your age: 56
+Your age is: 56
+
+
+Enter x: 9
+Enter y: 2
+Sum: 11
+Subtraction: 7
+Product: 18
+Divide: 4
+*/
