e0a7105b3fc94b0af091731f34908fca4ccbb50a
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 20:58:27 +0545

    comparision ops

---
+++ basics/Operators/comparisionOps.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<(5==5)<<endl; //1
+	cout<<(5!=5)<<endl; //0
+	cout<<(5>3)<<endl; //1
+	cout<<(5<3)<<endl; //0
+	cout<<(5>=4)<<endl; //1
+	cout<<(5<=3)<<endl; //0
+
+	return 0;
+}
