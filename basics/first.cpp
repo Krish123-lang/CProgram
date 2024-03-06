d26dea33c4f55a76c0dbe225b1acf9444a72b18b
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 18:53:01 +0545

    first code

---
+++ basics/first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  std::cout << "Hello World!\n";
+  std::cout<<"this is \t programmer here\n";
+  std::cout<<"this is \\ programmer here\n";
+  std::cout<<"this is \" programmer here\n";
+  return 0;
+}
+
+/*
+Hello World!
+Hello World!
+this is          programmer here
+this is \ programmer here
+this is " programmer here
+*/
