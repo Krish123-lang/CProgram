5192a260beb67ad6afa49d88be40f860a2e3abe0
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 19:46:23 +0545

    asciis and strings

---
+++ basics/asciis.cpp
@@ -0,0 +1,25 @@
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+	char a = 65, b = 66, c = 67;
+	cout << a;
+	cout << b;
+	cout << c;
+	return 0;
+}
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string greeting = "Hello";
+  cout << greeting;
+  return 0;
+}
