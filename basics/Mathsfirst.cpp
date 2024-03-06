1fb49eafb11215f7a88f8ae3b881ffa75f84ab52
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 22:26:29 +0545

    maths

---
+++ basics/Mathsfirst.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+
+{
+	cout<<min(3,5)<<endl; //3
+	cout<<max(3,5)<<endl; //5
+	cout<<sqrt(25)<<endl; //5
+	cout<<round(3.5)<<endl; //4
+	return 0;
+}
