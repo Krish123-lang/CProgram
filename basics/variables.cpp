033478ad8f376852c39f0dc03be1bb700ecf6f40
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 19:30:07 +0545

    variables

---
+++ basics/variables.cpp
@@ -0,0 +1,53 @@
+/*
+int - stores integers (whole numbers), without decimals, such as 123 or -123
+double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+string - stores text, such as "Hello World". String values are surrounded by double quotes
+bool - stores values with two states: true or false
+*/
+
+
+#include<iostream>
+#include<string>
+
+int main()
+{
+	/*
+	int var1;
+	var1=10; // 10
+	std::cout<<"The value of var1 is: "<<var1<<"\n";
+	
+	float f=10.101;
+	std::cout<<"The value of f is: "<<f<<"\n";
+
+	double d=10.101;
+	std::cout<<"The value of d is: "<<d<<"\n";
+
+	char c='A';
+	std::cout<<"The value of c is: "<<c<<"\n";
+
+	bool bool1=true;
+	std::cout<<bool1<<"\n";
+
+	bool cool1=false;
+	std::cout<<cool1<<"\n";
+	*/
+
+	//Multiple variable
+	/*
+	int x=10,y=20,z=30;
+	std::cout<<x+y+z; // 60
+	*/
+
+	/*
+	int x,y,z;
+	x=11,y=22,z=33;
+	std::cout<<x<<"\t"<<y<<"\t"<<z;
+	*/
+
+	//Constant
+	const float PI=3.14;
+	std::cout<<PI; // 3.14
+	
+	return 0;
+}
