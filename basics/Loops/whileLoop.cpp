bb6f4432f60b4709778d314c07f6e441a9155a8b
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 23:04:02 +0545

    multiplication table using while loop

---
+++ basics/Loops/whileLoop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=5, i=1;
+	while(i<=10)
+	{
+		cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+		i++;
+	}
+	return 0;
+}
+/*
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
+*/
