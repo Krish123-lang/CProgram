fbfc1936f950c744ec7e4a756b0d71e0e0fc36aa
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-03-06 23:08:45 +0545

    multiplication table using do While loop

---
+++ basics/Loops/doWhileLoop.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	int n=8,i=1;
+	do
+	{
+		cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+		i++;	
+	}
+	while(i<=10);
+	return 0;
+}
+/*
+8 * 1 = 8
+8 * 2 = 16
+8 * 3 = 24
+8 * 4 = 32
+8 * 5 = 40
+8 * 6 = 48
+8 * 7 = 56
+8 * 8 = 64
+8 * 9 = 72
+8 * 10 = 80
+
+*/
